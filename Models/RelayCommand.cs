ad3efe4f2e6161173e2284aac8a530e1f0c0d6f9
Branden Fitzwater <fitzwaterbranden@gmail.com>
2024-08-20 22:46:50 -0700

    FEAT: C# Solution initial commit

---
+++ Models/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+
+namespace Quickee.Models
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action? _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action? execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute == null)
+                return true;
+
+            return _canExecute.Invoke();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Quickee.Models;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Quickee.ViewModels
+{
+    class MainViewModel
+    {
+        public ICommand CloseWindowCommand { get; }
+
+        public MainViewModel()
+        {
+            CloseWindowCommand = new RelayCommand(() => Application.Current.Shutdown());
+        }
+    }
+}
