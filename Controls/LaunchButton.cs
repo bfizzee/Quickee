ccfd04e9b628368ed6e58fbcba712935b20b371e
Branden Fitzwater <fitzwaterbranden@gmail.com>
2024-08-29 20:07:32 -0700

    CHORE: Move ButtonInfo to models

--- Controls/LaunchButton.cs
+++ Controls/LaunchButton.cs
@@ -97,27 +97,4 @@ namespace Quickee.Controls
         private bool CanLaunch()
             => true;
     }
-
-    public class ButtonInfo
-    {
-        public string Name { get; set; }
-        public string LaunchPath { get; set; }
-        public string LaunchArgs { get; set; }
-        public string IconPath { get; set; }
-
-        public ButtonInfo() : this("", "", "", "") { }
-
-        public ButtonInfo(string name, string launchPath, string launchArgs = "", string iconPath = "")
-        {
-            Name = name;
-            LaunchPath = launchPath;
-            LaunchArgs = launchArgs;
-            IconPath = iconPath;
-        }
-
-        public override string ToString()
-        {
-            return $"Button {Name} | \"{LaunchPath}\" \"{LaunchArgs}\" | Icon: {IconPath}";
-        }
-    }
 }
---
+++ Models/ButtonInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace Quickee.Models
+{
+
+    public class ButtonInfo
+    {
+        public string Name { get; set; }
+        public string LaunchPath { get; set; }
+        public string LaunchArgs { get; set; }
+        public string IconPath { get; set; }
+
+        public ButtonInfo() : this("", "", "", "") { }
+
+        public ButtonInfo(string name, string launchPath, string launchArgs = "", string iconPath = "")
+        {
+            Name = name;
+            LaunchPath = launchPath;
+            LaunchArgs = launchArgs;
+            IconPath = iconPath;
+        }
+
+        public override string ToString()
+        {
+            return $"Button {Name} | \"{LaunchPath}\" \"{LaunchArgs}\" | Icon: {IconPath}";
+        }
+    }
+}
